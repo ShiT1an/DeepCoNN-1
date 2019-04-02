159fea3c4c511e9f80b93948adbece46d3e4ae24
unknown <18392086668@163.com>
2019-04-02 21:16:19 +0800

    'fixed'

--- custom_functions.py
+++ custom_functions.py
@@ -10,12 +10,12 @@ def init_embeddings_map(fname):
 
 def get_embed_and_pad_func(i_seq_len, u_seq_len, pad_value, embedding_map):
     def embed(row):
-        sentence = row["userReviews"].split()[:u_seq_len]
+        sentence = row["userReviews"].strip("[']").split("', '")[:u_seq_len]
         reviews = list(map(lambda word: embedding_map.get(word)
             if word in embedding_map else pad_value, sentence))
         row["userReviews"] = reviews +\
                 [pad_value] * (u_seq_len - len(reviews))
-        sentence = row["movieReviews"].split()[:i_seq_len]
+        sentence = row["movieReviews"].strip("[']").split("', '")[:i_seq_len]
         reviews = list(map(lambda word: embedding_map.get(word)
             if word in embedding_map else pad_value, sentence))
         row["movieReviews"] = reviews +\
@@ -29,3 +29,19 @@ def get_embed_aspects(aspects, embedding_map):
             if word in embedding_map else print('主题词未在Embedding_map中找到映射'), aspects))
     embed_aspects = np.array(embed_aspects)
     return embed_aspects
+
+
+def get_embed_and_pad_func_for_ian(rev_text_len, asp_term_len, pad_value, embedding_map):
+    def embed(row):
+        sentence = row["aspect_term"].strip("[']").split("', '")[:asp_term_len]
+        reviews = list(map(lambda word: embedding_map.get(word)
+            if word in embedding_map else pad_value, sentence))
+        row["aspect_term"] = reviews +\
+                [pad_value] * (asp_term_len - len(reviews))
+        sentence = row["review_text"].strip("[']").split("', '")[:rev_text_len]
+        reviews = list(map(lambda word: embedding_map.get(word)
+            if word in embedding_map else pad_value, sentence))
+        row["review_text"] = reviews +\
+                [pad_value] * (rev_text_len - len(reviews))
+        return row
+    return embed
